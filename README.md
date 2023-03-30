e93f21d19910fdd7b28a03471ce3145c9e85ea04
Christian Barcelona <christianbarcelona95@gmail.com>
2023-03-30 18:57:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Blogr homepage
+
+Blogr landing page.
+
+![Image preview](design/desktop-design.jpg)
+
+## Live link
+
+https://9cb5.github.io/blogr-v2/
+
+## Installation
+
+Run the command below to install the dependencies.
+
+```bash
+npm install
+```
+
+## Usage
+
+To run the project locally and compile changes, run the following command
+
+```bash
+npm run compile:sass
+```
---
+++ script.js
@@ -0,0 +1,76 @@
+// ================================================================================================
+function toggleMenu() {
+    const hamburger = document.querySelector(".navigation__hamburger");
+    const menu = document.querySelector(".navigation__links");
+
+    // Show image
+    // --------------------------------------------------------------------------------------------
+    menu.classList.toggle("show");
+
+    // Toggle nav icon
+    // --------------------------------------------------------------------------------------------
+    const src = hamburger.src;
+    let newSrc = "";
+
+    newSrc = src.includes("hamburger") ? src.replace("hamburger", "close") : newSrc = src.replace("close", "hamburger");
+    hamburger.src = newSrc;
+}
+
+// ================================================================================================
+function togglePanel(panelNumber) {
+    const panel = document.getElementById(`panel-${panelNumber}`);
+    const arrow = document.getElementById(`arrow-${panelNumber}`);
+
+    // Close any open panels before opening one
+    // --------------------------------------------------------------------------------------------
+    for (let i = 0; i < 3; i++) {
+        const panelToClose = document.getElementById(`panel-${i}`);
+        const arrowToRotate = document.getElementById(`arrow-${i}`);
+
+        if (i !== panelNumber && panelToClose.classList.contains("show") && arrowToRotate.classList.contains("rotate")) {
+
+            panelToClose.classList.remove("show");
+            arrowToRotate.classList.remove("rotate");
+        }
+    }
+
+    // Open the panel and rotate arrow
+    // --------------------------------------------------------------------------------------------
+    panel.classList.toggle("show");
+    arrow.classList.toggle("rotate");
+}
+
+// ================================================================================================
+function animationSetup() {
+    const observedElements = [
+        { id: "first-observe", animation: "animate__fadeInLeft" },
+        { id: "second-observe", animation: "animate__fadeInRight" },
+        { id: "third-observe", animation: "animate__fadeIn" },
+    ];
+
+    // Checks to see if certain elements are in viewport and if so, trigger animation
+    // --------------------------------------------------------------------------------------------
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const intersecting = entry.isIntersecting;
+            const index = observedElements.findIndex(x => x.id === entry.target.id);
+
+            if (intersecting && index > -1) {
+                entry.target.classList.add("animate__animated", observedElements[index].animation);
+            }
+        })
+    })
+
+    // Setup observed elements
+    // --------------------------------------------------------------------------------------------
+    observedElements.forEach(i => {
+        observer.observe(document.getElementById(i.id));
+    });
+}
+
+// ================================================================================================
+function main() {
+   animationSetup();
+}
+
+main();
